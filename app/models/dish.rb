7ced6678790488270140416e83f26d3846ae146b
Benito Miranda <benomir82@gmail.com>
2016-01-23 22:35:55 -0600

    Add has many items relationship

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,5 +1,6 @@
 class Dish < ActiveRecord::Base
   belongs_to :restaurant
+  has_many :items
   validates :dish_group, :name, :description, :price, :photo, :restaurant_id,
   					presence: true
 end
