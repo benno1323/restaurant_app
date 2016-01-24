ba015280a2c6da6b2e57ef3b7be37aaad3eda78d
Benito Miranda <benomir82@gmail.com>
2016-01-23 20:14:22 -0600

    Change table name from orders to dishes

--- db/migrate/20160124020920_add_precision_and_scale_to_price.rb
+++ db/migrate/20160124020920_add_precision_and_scale_to_price.rb
@@ -1,5 +1,5 @@
 class AddPrecisionAndScaleToPrice < ActiveRecord::Migration
   def change
-  	change_column :orders, :price, :decimal, precision: 8, scale: 2
+  	change_column :dishes, :price, :decimal, precision: 8, scale: 2
   end
 end
