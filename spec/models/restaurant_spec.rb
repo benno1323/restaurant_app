b4523538a46a173c65e44044268bd52df80aff9b
Benito Miranda <benomir82@gmail.com>
2016-01-23 18:32:08 -0600

    Add unit tests for Restaurant model

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	before(:all) do
+		@restaurant = build(:restaurant)
+	end
+
+  it 'is valid with a name, description, address, phone and logo' do
+  	expect(@restaurant).to be_valid
+  end
+
+  it 'is invalid without a name' do
+  	@restaurant.name = nil
+  	@restaurant.valid?
+  	expect(@restaurant.errors[:name]).to include("can't be blank")
+  end
+
+  it 'is invalid without a description' do
+  	@restaurant.description = nil
+  	@restaurant.valid?
+  	expect(@restaurant.errors[:description]).to include("can't be blank")
+  end
+
+  it 'is invalid without an address' do
+  	@restaurant.address = nil
+  	@restaurant.valid?
+  	expect(@restaurant.errors[:address]).to include("can't be blank")
+  end
+
+  it 'is invalid without a phone' do
+  	@restaurant.phone = nil
+  	@restaurant.valid?
+  	expect(@restaurant.errors[:phone]).to include("can't be blank")
+  end
+
+  it 'is invalid without a logo' do
+  	@restaurant.logo = nil
+  	@restaurant.valid?
+  	expect(@restaurant.errors[:logo]).to include("can't be blank")
+  end
 end
