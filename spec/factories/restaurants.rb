4777481b7dc94e4589d4d538fc58fadd83c6afb1
Benito Miranda <benomir82@gmail.com>
2016-01-23 22:27:26 -0600

    Update tests to include relationship with orders and dishes

--- spec/factories/restaurants.rb
+++ spec/factories/restaurants.rb
@@ -5,5 +5,25 @@ FactoryGirl.define do
 		address 		{ Faker::Address.street_address }
 		phone 			{ Faker::PhoneNumber.phone_number }
 		logo 				"image.jpg"
+
+		factory :restaurant_with_orders do
+			transient do
+				orders_count 5
+			end
+
+			after(:create) do |restaurant, evaluator|
+				FactoryGirl.create_list(:order, evaluator.orders_count, restaurant: restaurant)
+			end
+		end
+
+		factory :restaurant_with_dishes do
+			transient do
+				dishes_count 5
+			end
+
+			after(:create) do |restaurant, evaluator|
+				FactoryGirl.create_list(:dish, evaluator.dishes_count, restaurant: restaurant)
+			end
+		end
 	end
 end
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -38,4 +38,14 @@ RSpec.describe Restaurant, type: :model do
   	@restaurant.valid?
   	expect(@restaurant.errors[:logo]).to include("can't be blank")
   end
+
+  it 'has a number of orders' do
+    restaurant = create(:restaurant_with_orders)
+    expect(restaurant.orders.count).to eq(5)
+  end
+
+  it 'has a number of dishes' do
+    restaurant = create(:restaurant_with_dishes)
+    expect(restaurant.dishes.count).to eq(5)
+  end
 end
