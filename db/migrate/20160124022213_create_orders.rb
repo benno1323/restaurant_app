264c1bfc7fa2430e2371e5b369d2d13b1e8b23d4
Benito Miranda <benomir82@gmail.com>
2016-01-23 20:24:58 -0600

    Add precision, scale, default and null values

--- db/migrate/20160124022213_create_orders.rb
+++ db/migrate/20160124022213_create_orders.rb
@@ -2,9 +2,9 @@ class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
       t.date :date
-      t.decimal :sub_total
-      t.decimal :taxes
-      t.decimal :total
+      t.decimal :sub_total, precision: 8, scale: 2, default: 0.0, null: false
+      t.decimal :taxes, precision: 8, scale: 2, default: 0.0, null: false
+      t.decimal :total, precision: 8, scale: 2, default: 0.0, null: false
       t.references :restaurant, index: true, foreign_key: true
 
       t.timestamps null: false
