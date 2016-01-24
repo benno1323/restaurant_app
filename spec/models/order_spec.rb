ab7a750ae84f387b07be6f2c78b318aa947f458b
Benito Miranda <benomir82@gmail.com>
2016-01-23 21:53:42 -0600

    Add unit tests for Order model

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	before(:all) do
+		@order = build(:order)
+	end
+
+  it 'is valid with a date, sub_total, taxes, total and restaurant_id' do
+  	expect(@order).to be_valid
+  end
+
+  it 'is invalid without a date' do
+  	@order.date = nil
+  	@order.valid?
+  	expect(@order.errors[:date]).to include("can't be blank")
+  end
+
+  it 'is invalid without a sub_total' do
+  	@order.sub_total = nil
+  	@order.valid?
+  	expect(@order.errors[:sub_total]).to include("can't be blank")
+  end
+
+  it 'is invalid without taxes' do
+  	@order.taxes = nil
+  	@order.valid?
+  	expect(@order.errors[:taxes]).to include("can't be blank")
+  end
+
+  it 'is invalid without a restaurant_id' do
+  	@order.restaurant_id = nil
+  	@order.valid?
+  	expect(@order.errors[:restaurant_id]).to include("can't be blank")
+  end
 end
