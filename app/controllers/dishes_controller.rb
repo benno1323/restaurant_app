1dcf81c565f93ea94eea7fd476e2fa9ab138e7bf
Benito Miranda <benomir82@gmail.com>
2016-01-24 14:10:22 -0600

    Pass dishes controller specs

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,2 +1,55 @@
 class DishesController < ApplicationController
+	before_action :load_restaurant
+	before_action :find_restaurant_dish, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@dishes = @restaurant.dishes
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def new
+		@dish = @restaurant.dishes.build
+	end
+
+	def create
+		@dish = @restaurant.dishes.build(dish_params)
+
+		if @dish.save
+			redirect_to(@restaurant)
+		else
+			render 'restaurants/show'
+		end
+	end
+
+	def update
+		if @dish.update(dish_params)
+			redirect_to(restaurant_dish_path(@restaurant, @dish))
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@dish.destroy
+		redirect_to(@restaurant)
+	end
+
+	private
+
+	def find_restaurant_dish
+		@dish = @restaurant.dishes.find(params[:id])
+	end
+
+	def load_restaurant
+		@restaurant = Restaurant.find(params[:restaurant_id])
+	end
+
+	def dish_params
+		params.require(:dish).permit(:dish_group, :name, :description, :price, :photo)
+	end
 end
