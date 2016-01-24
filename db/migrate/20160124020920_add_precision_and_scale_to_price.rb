1111f585243c27649039fc44f0cafedebf108538
Benito Miranda <benomir82@gmail.com>
2016-01-23 20:15:56 -0600

    Add default and null values

--- db/migrate/20160124020920_add_precision_and_scale_to_price.rb
+++ db/migrate/20160124020920_add_precision_and_scale_to_price.rb
@@ -1,5 +1,6 @@
 class AddPrecisionAndScaleToPrice < ActiveRecord::Migration
   def change
-  	change_column :dishes, :price, :decimal, precision: 8, scale: 2
+  	change_column :dishes, :price, :decimal, precision: 8, scale: 2,
+  								default: 0.0, null: false
   end
 end
