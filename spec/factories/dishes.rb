190a60d855b42043afc5a8555cb035fce572eda8
Benito Miranda <benomir82@gmail.com>
2016-01-23 20:05:58 -0600

    Set Dish factory

--- spec/factories/dishes.rb
+++ spec/factories/dishes.rb
@@ -1,11 +1,11 @@
 FactoryGirl.define do
   factory :dish do
-    dish_group "MyString"
-name "MyString"
-description "MyText"
-price "9.99"
-photo "MyString"
-restaurant nil
+  	association :restaurant
+		sequence(:dish_group) { |n| "Dish Group #{n}" }
+		sequence(:name) { |n| "Dish #{n}" }
+		description "MyText"
+		price "9.99"
+		photo "MyString"
   end
 
 end
