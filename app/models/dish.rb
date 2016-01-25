7be661dc331bdc6dee89470706ac5abd2302183f
Benito Miranda <benomir82@gmail.com>
2016-01-25 11:56:09 -0600

    Remove photo from validation

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,7 +1,7 @@
 class Dish < ActiveRecord::Base
   belongs_to :restaurant
   has_many :items
-  validates :dish_group, :name, :description, :price, :photo, :restaurant_id,
+  validates :dish_group, :name, :description, :price, :restaurant_id,
   					presence: true
 
   mount_uploader :photo, PhotoUploader
