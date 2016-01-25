1f29b8ed8e97c31ac6668452b77b7ad11fbbbe61
Benito Miranda <benomir82@gmail.com>
2016-01-24 23:12:00 -0600

    Mount logo uploader

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,6 @@ class Restaurant < ActiveRecord::Base
 	has_many :dishes, dependent: :destroy
 	has_many :orders, dependent: :destroy
 	validates :name, :description, :address, :phone, :logo, presence: true
+
+	mount_uploader :logo, LogoUploader
 end
