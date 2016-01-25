2cb11e0065d26a0b1aa282ecb93b0cca7f5c152e
Benito Miranda <benomir82@gmail.com>
2016-01-25 11:38:37 -0600

    Set default restaurant logo

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,7 @@
 class Restaurant < ActiveRecord::Base
 	has_many :dishes, dependent: :destroy
 	has_many :orders, dependent: :destroy
-	validates :name, :description, :address, :phone, :logo, presence: true
+	validates :name, :description, :address, :phone, presence: true
 
 	mount_uploader :logo, LogoUploader
 end
