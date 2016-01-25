1575ee38265030a0f7464ebea6145c976c59e6bc
Benito Miranda <benomir82@gmail.com>
2016-01-25 12:56:14 -0600

    Set fog_public to true

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -7,7 +7,7 @@ CarrierWave.configure do |config|
     :host                   => 'nil',             # optional, defaults to nil
     :endpoint               => 'nil' # optional, defaults to nil
   }
-  config.fog_directory  = 'dalaitest'                     # required
-  config.fog_public     = false                                   # optional, defaults to true
+  config.fog_directory  = 'pruebaruby'                     # required
+  config.fog_public     = true                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
