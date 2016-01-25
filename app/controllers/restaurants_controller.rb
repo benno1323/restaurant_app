77c4a2f544cfe455b1d90f9ebe9fbfc398397611
Benito Miranda <benomir82@gmail.com>
2016-01-25 10:22:28 -0600

    Authenticate admin

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,6 @@
 class RestaurantsController < ApplicationController
 	before_action :find_restaurant, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_admin!, except: [:index, :show]
 
 	def index
 		@restaurants = Restaurant.all
