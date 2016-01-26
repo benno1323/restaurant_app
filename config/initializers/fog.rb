53ea41bc3c0028d1af8d2f2455154fdb5a57042e
Benito Miranda <benomir82@gmail.com>
2016-01-25 22:00:47 -0600

    Final Fog Setup

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -4,10 +4,10 @@ CarrierWave.configure do |config|
     :aws_access_key_id      => ENV['S3_ACCESS_KEY'],                        # required
     :aws_secret_access_key  => ENV['S3_ACCESS_PASSWORD'],                        # required
     :region                 => 'us-west-2',                  # optional, defaults to 'us-east-1'
-    :host                   => 'nil',             # optional, defaults to nil
-    :endpoint               => 'nil' # optional, defaults to nil
+    :host                   => nil,             # optional, defaults to nil
+    :endpoint               => nil # optional, defaults to nil
   }
   config.fog_directory  = 'pruebaruby'                     # required
-  config.fog_public     = true                                   # optional, defaults to true
+  config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
